c32767b235c536ca49baac46f68fb8c420834201
Filip Mikoviny <filip.mikoviny@epfl.ch>
2023-09-10 14:22:14 +0200

    raymarch loop works correctly I think

--- res/shaders/clouds.frag.glsl
+++ res/shaders/clouds.frag.glsl
@@ -20,7 +20,7 @@ uniform float u_Time;
 
 in vec2 v_TexCoord;
 
-const int NUMBER_OF_STEPS = 128;
+const int NUMBER_OF_STEPS = 64;
 const float MINIMUM_HIT_DISTANCE = 0.001;
 const float MAXIMUM_TRACE_DISTANCE = 10000.0;
 
@@ -29,7 +29,7 @@ const vec3 SPHERES[3] = vec3[](vec3(2.0, -1.0, 0.0), vec3(0.0, -1.0, 0.0), vec3(
 const vec3 LIGHT_COLOR = vec3(1., 1., 1.);
 const vec3 LIGHT_DIR = normalize(vec3(0.0, -1.0, 0.5));
 const float LIGHT_INTENSITY = 20;
-const vec3 BGC = vec3(0.572, 0.772, 0.921);
+const vec3 BGC = vec3(0.572, 0.772, 0.921)*0.7;
 
 #define PI 3.14159265358979323846
 float jitter;
@@ -73,6 +73,15 @@ float fbm( vec3 p )
     return f;
 }
 
+//used to switch smoothly between sdfs to render, https://www.shadertoy.com/view/WdXGRj
+//time, duration of a single SDF, transition time to the next SDF, already included in duration
+float stepUp(float t, float duration, float transition_duration)
+{
+  float cur_time = mod(t += transition_duration, duration);
+  float stp = floor(t / duration) - 1.0;
+  return smoothstep(0.0, transition_duration, cur_time) + stp;
+}
+
 
 // the Henyey-Greenstein phase function
 float phase(float g, vec3 view_dir, vec3 light_dir)
@@ -83,9 +92,15 @@ float phase(float g, vec3 view_dir, vec3 light_dir)
 
 float map(vec3 currPos);
 
-float distance_from_sphere(in vec3 p, in vec3 c, float r)
+float sdSphere(in vec3 p, in vec3 c, float r)
 {
     return length(p - c) - r;
+}
+
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
 }
 
 float distanceFromPlane(in vec3 p){
@@ -109,7 +124,7 @@ float intersect_volume(in vec3 ro, in vec3 rd, float maxT = 15)
 
 //https://shaderbits.com/blog/creating-volumetric-ray-marcher
 
-vec4 integrate(in vec3 ro, in vec3 rd)
+vec3 integrate(in vec3 ro, in vec3 rd)
 {
     float step_size = 0.2;
     int max_steps = NUMBER_OF_STEPS;
@@ -119,10 +134,6 @@ vec4 integrate(in vec3 ro, in vec3 rd)
     float sigma_s = 0.9; //scattering
     float sigma_t = sigma_a + sigma_s; //extinction coeff
     float phase_g = 0.8; //phase function g factor
-
-    Ray vr = Ray(ro, rd);
-    Sphere sphere = Sphere(SPHERES[0], 1., 0);
-    Hit view_sphere_hit;
 
     float curDensity = 0;
     float transmittance = 1.;
@@ -162,26 +173,29 @@ vec4 integrate(in vec3 ro, in vec3 rd)
                 shadow += lsample;
             }
             //curDensity = clamp((curSample * density = 1/max_steps) * 20 , 0.0, 1.0);
-            curDensity = clamp((curSample * density), 0.0, 1.0);
+            curDensity = clamp((curSample), 0.0, 1.0) * density;
             float light_attenuation = exp(-shadow / shadow_steps * sigma_t);
-            color += LIGHT_COLOR * light_attenuation * curDensity * transmittance;
+            color += light_attenuation * curDensity * transmittance * sigma_s;
+
             transmittance *= 1.-curDensity;
+            //float sample_transparency = exp(- curSample * density * sigma_t);
+            //transmittance *= sample_transparency;
 
             color += exp(-map(pos + vec3(0,0.25,0.0)) * .2) * curDensity * vec3(0.15, 0.45, 1.1) * transmittance;
         }
         pos += rd * step_size;
     }
-    //return sum;
-    return vec4(color, transmittance);
+    return LIGHT_COLOR * color + BGC*transmittance;
 }
 
 //we don't need high precision for deciding when we hit the volume
 //if we do 1-d, we can use this function as a density function as well?
 float map(vec3 currPos) {
-    float minv = distance_from_sphere(currPos,SPHERES[0], 1.0);
+    float distortion = fbm(currPos);
+    float minv = sdSphere(currPos,SPHERES[0], 1.0) + distortion;
     return 1-minv;
     for(int i = 1; i < SPHERES.length; i++) {
-        float d = distance_from_sphere(currPos,SPHERES[i], 1.0);
+        float d = sdSphere(currPos,SPHERES[i], 1.0);
         //minv = smooth_min(minv, d, 2.0) + fbm3(currPos);
     }
     return minv;
@@ -222,9 +236,10 @@ void main()
     // ray direction
     vec3 rd = ca * normalize( vec3(p,fl) );
 
-    vec4 shaded_color = integrate(ro, rd);
-    color = shaded_color;
-    //color = vec4(shaded_color, 1.0);
+    vec3 shaded_color = integrate(ro, rd);
+    //gamma correction
+    shaded_color = pow(shaded_color, vec3(1.0/2.2));
+    color = vec4(shaded_color, 1.0);
 }
 
 
